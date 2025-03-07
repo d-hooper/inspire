768689875a03162277d0c5b4811c1dfc3422ce93
d-hooper <dallinhoop3@gmail.com>
2025-03-07 15:59:31 -0700

    drawing time to screen initially

--- app/controllers/WeatherController.js
+++ app/controllers/WeatherController.js
@@ -3,11 +3,12 @@ import { weatherService } from "../services/WeatherService.js";
 import { Pop } from "../utils/Pop.js";
 
 export class WeatherController {
-  
+
   constructor() {
     console.log('🌡️☁️ controller');
     AppState.on('weatherCity', this.drawWeather)
     this.getWeather()
+    this.drawTime()
   }
 
   drawWeather() {
@@ -17,6 +18,12 @@ export class WeatherController {
     weatherElem.innerHTML = weatherContent
   }
 
+  drawTime() {
+    const time = new Date().toLocaleTimeString()
+    console.log(time);
+    const timeElem = document.getElementById('time')
+    timeElem.innerText = time.toString()
+  }
 
   async getWeather() {
     try {
